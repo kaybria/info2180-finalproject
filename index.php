1cf0dbe6c9fde5d561b43a0a15ffa786884ae3f1
Kali Soutar <kalisoutar@gmail.com>
2021-12-04 20:14:15 -0500

    updated logout from Abe

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                     <a class="active" data-toggle="tab" href="#home"><i class="fa fa-home"></i>Home</a> 
                     <a data-toggle="tab" href="#add"><i class="fa fa-plus-circle"  ></i>Add User</a>
                     <a data-toggle="tab" href="#new"><i class="fa fa-plus-circle"  ></i>New Issue</a>
-                    <a data-toggle="tab" href="#logout"><i class='fa fa-power-off'></i>Logout</a>
+                    <a   href = "./logout.php"><i class='fa fa-power-off'></i>Logout</a>
                 </ul>
             </div>
         </div>  
@@ -114,51 +114,44 @@
                 </div>
 
                 <div id="new" class="tab-pane fade">
-                <h3>Create Issue</h3>
-                <form method="post" name="form" id="form" action="">
-                <label for="title">Title</label>
-                <input type="text" id="title" name="array[]" placeholder=" ">
-            
-                <label for="description">Description</label>
-                <textarea id="description" name="array[]" placeholder=" " style="height:200px"></textarea>
-                        
-                <label for="assigned">Assigned To</label>
-                <select id="assigned" name="assigned">
-                    <option value="name1">Admin</option>
-                    <option value="name2">S</option>
+                        <h3>Create Issue</h3>
+                        <form method="post" name="form" id="form" action="">
+                        <label for="title">Title</label>
+                        <input type="text" id="title" name="array[]" placeholder=" ">
                     
-                </select><br><br>
-            
-                <label for="type">Type</label>
-                <select id="type" name="type">
-                   <option value="bug">Bug</option>
-                   <option value="proposal"> Proposal </option>
-                   <option value="task"> Task </option>
-                </select><br><br>
-            
-                <label for="priority">Priority</label>
-                <select id="priority" name="priority">
-                    <option value="minor">Minor</option>
-                    <option value="major">Major</option>
-                    <option value="critical">Critical</option>
-                </select><br>
-                        
-                <button id="issue_submit" value="Submit">Submit</button>
+                        <label for="description">Description</label>
+                        <textarea id="description" name="array[]" placeholder=" " style="height:200px"></textarea>
+                                
+                        <label for="assigned">Assigned To</label>
+                        <select id="assigned" name="assigned">
+                            <option value="name1">Admin</option>
+                            <option value="name2">S</option>
+                            
+                        </select><br><br>
                     
-            </form>
-                    </div>
+                        <label for="type">Type</label>
+                        <select id="type" name="type">
+                        <option value="bug">Bug</option>
+                        <option value="proposal"> Proposal </option>
+                        <option value="task"> Task </option>
+                        </select><br><br>
+                    
+                        <label for="priority">Priority</label>
+                        <select id="priority" name="priority">
+                            <option value="minor">Minor</option>
+                            <option value="major">Major</option>
+                            <option value="critical">Critical</option>
+                        </select><br>
+                                
+                        <button id="issue_submit" value="Submit">Submit</button>
+                            
+                    </form>
+                </div>
                 
                      
-                
-                  </form>
-                
-                  <div id="logout" class="tab-pane fade">
-                  <a href = "./logout.php"><h3>Logout</h3> </a>
-                </div>
+            
             </div>
-        </div>
-
-                
+        </div>               
         
         
     </div>
